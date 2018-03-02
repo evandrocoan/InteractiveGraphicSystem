adb47cc746ad3f22a4ec4d69f3018a0f12808931
evandrocoan <evandrocoan@hotmail.com>
2018-03-02 01:09:54 -0300

    Removed useless comments on helloworld_gtk.cpp helloworld_gtkmm.cpp

--- helloworld_gtkmm.cpp
+++ helloworld_gtkmm.cpp
@@ -4,9 +4,6 @@
  * To build it run:
  *     g++ helloworld2.cpp -o helloworld2 `pkg-config gtkmm-3.0 --cflags --libs`
  *
- * To install GTKmm on windows, you will need to use MSYS2. See:
- * https://wiki.gnome.org/Projects/gtkmm/MSWindows
- *
  */
 
 #include <gtkmm.h>
