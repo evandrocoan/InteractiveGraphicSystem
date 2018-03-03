58174128b9f8c5537aa1b534772daaff7bf96f27
evandrocoan <evandrocoan@hotmail.com>
2018-03-03 16:07:00 -0300

    Implemented a time stamp 7.484e+003 7.484e+003 to be used when
    outputting the logging statements. Example:
    0.000e+000 0.000e+000 src/main.cpp/main:9 Starting the main application...
    7.484e+003 7.484e+003 src/mainwindow.cpp/MainWindow:17 Draw options
    5.016e-001 0.000e+000 src/mainwindow.cpp/MainWindow:22 Adding the draw options box to left frame
    2.467e+002 2.470e+002 src/mainwindow.cpp/MainWindow:25 Viewport
    5.004e-001 1.000e+000 src/mainwindow.cpp/MainWindow:30 Show all components
    5.005e-001 0.000e+000 src/main.cpp/main:14 Shows the main window and returns when it is closed.

---
+++ src/debugger.cpp
@@ -0,0 +1,34 @@
+/*********************** Licensing *******************************************************
+*
+*   Copyright 2018 @ Evandro Coan, https://github.com/evandrocoan
+*
+*  This program is free software; you can redistribute it and/or modify it
+*  under the terms of the GNU General Public License as published by the
+*  Free Software Foundation; either version 3 of the License, or ( at
+*  your option ) any later version.
+*
+*  This program is distributed in the hope that it will be useful, but
+*  WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+*  General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*****************************************************************************************
+*/
+
+
+#include "debugger.h"
+
+// [TUT] How to use an efficient debug system, which does not overload the final code -> C/C++ Language Implementation
+// https://forums.alliedmods.net/showthread.php?t=277682#Cpp
+//
+// How do I use extern to share variables between source files?
+// https://stackoverflow.com/questions/1433204/how-do-i-use-extern-to-share-variables-between-source-files
+#if DEBUG_LEVEL > DEBUG_LEVEL_DISABLED_DEBUG
+  // initialize `extern` variables shared across all source files
+  std::clock_t _debugger_current_saved_c_time = std::clock();
+  std::chrono::time_point<std::chrono::high_resolution_clock> _debugger_current_saved_chrono_time = std::chrono::high_resolution_clock::now();
+#endif
+
