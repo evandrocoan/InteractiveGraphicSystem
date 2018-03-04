4812d0041e1ccf6245b9612e2cade21ba00f3080
evandrocoan <evandrocoan@hotmail.com>
2018-03-03 22:07:21 -0300

    Implemented a time stamp on src/debugger.h LOG function based on
    timestamp_example.cpp file.

--- src/debugger.h
+++ src/debugger.h
@@ -106,7 +106,26 @@
     { \
       std::clock_t c_end = std::clock(); \
       auto t_end = std::chrono::high_resolution_clock::now(); \
-      std::cout << tfm::format( "%.3e %.3e %s/%s:%s ", \
+      std::chrono::time_point< std::chrono::system_clock > now = std::chrono::system_clock::now();  \
+      auto duration = now.time_since_epoch(); \
+      /* typedef std::chrono::duration< int, std::ratio_multiply< std::chrono::hours::period, std::ratio< 21 > >::type > Days; */ \
+      /* Days days = std::chrono::duration_cast< Days >( duration ); */ \
+      /* duration -= days; */ \
+      auto hours = std::chrono::duration_cast< std::chrono::hours >( duration ); \
+      duration -= hours; \
+      auto minutes = std::chrono::duration_cast< std::chrono::minutes >( duration ); \
+      duration -= minutes; \
+      auto seconds = std::chrono::duration_cast< std::chrono::seconds >( duration ); \
+      duration -= seconds; \
+      auto milliseconds = std::chrono::duration_cast< std::chrono::milliseconds >( duration ); \
+      duration -= milliseconds; \
+      auto microseconds = std::chrono::duration_cast< std::chrono::microseconds >( duration ); \
+      /* duration -= microseconds; */ \
+      /* auto nanoseconds = std::chrono::duration_cast< std::chrono::nanoseconds >( duration ); */ \
+      time_t theTime = time(NULL); \
+      struct tm *aTime = localtime(&theTime); \
+      std::cout << tfm::format( "%02d:%02d:%02d:%03d:%03d %.3e %.3e %s/%s:%s ", \
+              aTime->tm_hour, minutes.count(), seconds.count(), milliseconds.count(), microseconds.count(), /* nanoseconds.count(), */ \
               std::chrono::duration<double, std::milli>(t_end-_debugger_current_saved_chrono_time).count(), \
               (1000.0 * (c_end - _debugger_current_saved_c_time)) / CLOCKS_PER_SEC, \
               __FILE__, __FUNCTION__, __LINE__ \
