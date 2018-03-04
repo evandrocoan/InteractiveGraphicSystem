3b53b9c4ad4b369cda3a77e6284a245620b10d74
evandrocoan <evandrocoan@hotmail.com>
2018-03-04 12:58:45 -0300

    Set to display the exit code on src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,13 @@
 int main (int argc, char *argv[])
 {
   LOG(1, "Starting the main application...");
-  Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv);
 
+  Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv);
   MainWindow mainwindow;
 
   LOG(1, "Shows the main window and returns when it is closed.");
-  return app->run(mainwindow);
+  int exit_code = app->run(mainwindow);
+
+  LOG(1, "exit_code: %d", exit_code);
+  return exit_code;
 }
