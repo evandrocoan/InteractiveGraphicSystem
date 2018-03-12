2a9c899e977ec2bc31d2035288f3ce6c39ca3429
evandrocoan <evandrocoan@hotmail.com>
2018-03-12 12:15:59 -0300

    Renamed the attribute Coordinates to coordinates and some other
    minor changes.

--- src/line.cpp
+++ src/line.cpp
@@ -3,8 +3,8 @@
 Line::Line(string name, Coordinate* line_cord1, Coordinate* line_cord2)
     : DrawableObject(name)
 {
-  Coordinates.push_back(line_cord1);
-  Coordinates.push_back(line_cord2);
+  coordinates.push_back(line_cord1);
+  coordinates.push_back(line_cord2);
 }
 
 Line::~Line()
@@ -13,5 +13,5 @@ Line::~Line()
 
 list<Coordinate*> Line::getCoordinates()
 {
-  return Coordinates;
+  return coordinates;
 }
