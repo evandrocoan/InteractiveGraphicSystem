67d068925a1e47249f6ff9f50a1ff54f48689835
evandrocoan <evandrocoan@hotmail.com>
2018-03-17 17:46:00 -0300

    Refactored the class file src/transformation.cpp into smaller units
    for its biggest functions.

--- src/transformation.cpp
+++ src/transformation.cpp
@@ -1,5 +1,11 @@
 #include "transformation.h"
 
+void Transformation::apply(Coordinate* point)
+{
+  LOG(4, "Applying transformation %s on %s", this->_transformation, *point);
+  point->multiply(&this->_transformation);
+}
+
 /**
  * Create and configure correctly a rotation.
  *
@@ -70,121 +76,21 @@ void Transformation::set_geometric_center(Coordinate center = _default_coordinat
 
       case TransformationType::SCALING:
       {
-        MatrixForm move_to_center
-        {
-          {1             ,  0            ,              0},
-          {0             ,  1            ,              0},
-          {-center.getx(), -center.gety(), -center.getz()}
-        };
-
-        if( index == 0 )
-        {
-          this->_transformation = move_to_center;
-        }
-        else
-        {
-          this->_transformation.multiply(&move_to_center);
-        }
-
-        // Do the scaling on the origin
-        this->_transformation.multiply(&transformation_data.matrix);
-
-        // Move back to its origin
-        move_to_center[3][0] = center.getx();
-        move_to_center[3][1] = center.gety();
-        move_to_center[3][2] = center.getz();
-
-        this->_transformation.multiply(&move_to_center);
+        this->_set_scaling_data(transformation_data, index, center);
         break;
       }
 
       case TransformationType::ROTATION:
       {
-        switch(transformation_data.rotation_type)
-        {
-          case RotationType::ON_ITS_OWN_CENTER:
-          {
-            MatrixForm move_to_center
-            {
-              {1             ,  0            ,              0},
-              {0             ,  1            ,              0},
-              {-center.getx(), -center.gety(), -center.getz()}
-            };
+        this->_set_scaling_data(transformation_data, index, center);
+        break;
+      }
 
-            if( index == 0 )
-            {
-              this->_transformation = move_to_center;
-            }
-            else
-            {
-              this->_transformation.multiply(&move_to_center);
-            }
-
-            // Do the rotation on the origin
-            this->_transformation.multiply(&transformation_data.matrix);
-
-            // Move back to its origin
-            move_to_center[3][0] = center.getx();
-            move_to_center[3][1] = center.gety();
-            move_to_center[3][2] = center.getz();
-
-            this->_transformation.multiply(&move_to_center);
-            break;
-          }
-
-          case RotationType::ON_WORLD_CENTER:
-          {
-            // Just rotate it, as all rotations are based on the world center
-            if( index == 0 )
-            {
-              this->_transformation = transformation_data.matrix;
-            }
-            else
-            {
-              this->_transformation.multiply(&transformation_data.matrix);
-            }
-            break;
-          }
-
-          case RotationType::ON_GIVEN_COORDINATE:
-          {
-            auto rotation_center = transformation_data.rotation_center;
-
-            MatrixForm move_to_center
-            {
-              {1                      ,  0                     ,                      0},
-              {0                      ,  1                     ,                      0},
-              {-rotation_center.getx(), -rotation_center.gety(),-rotation_center.getz()}
-            };
-
-            if( index == 0 )
-            {
-              this->_transformation =move_to_center;
-            }
-            else
-            {
-              this->_transformation.multiply(&move_to_center);
-            }
-
-            // Do the rotation on the origin
-            this->_transformation.multiply(&transformation_data.matrix);
-
-            // Move back to its origin
-            move_to_center[3][0] = rotation_center.getx();
-            move_to_center[3][1] = rotation_center.gety();
-            move_to_center[3][2] = rotation_center.getz();
-
-            this->_transformation.multiply(&move_to_center);
-            break;
-          }
-
-          default:
-          {
-            LOG(1, "");
-            LOG(1, "");
-            LOG(1, "ERROR! Invalid RotationType used: %d", transformation_data.rotation_type);
-          }
-        }
+      default:
+      {
+        LOG(1, "");
+        LOG(1, "");
+        LOG(1, "ERROR! Invalid TransformationData type used: %d", transformation_data.type);
       }
     }
 
@@ -192,9 +98,131 @@ void Transformation::set_geometric_center(Coordinate center = _default_coordinat
   }
 }
 
-void Transformation::apply(Coordinate* point)
+void Transformation::_set_scaling_data(TransformationData &transformation_data, unsigned int &index, Coordinate &center)
 {
-  LOG(4, "Applying transformation %s on %s", this->_transformation, *point);
-  point->multiply(&this->_transformation);
+  MatrixForm move_to_center
+  {
+    {1             ,  0            ,              0},
+    {0             ,  1            ,              0},
+    {-center.getx(), -center.gety(), -center.getz()}
+  };
+
+  if( index == 0 )
+  {
+    this->_transformation = move_to_center;
+  }
+  else
+  {
+    this->_transformation.multiply(&move_to_center);
+  }
+
+  // Do the scaling on the origin
+  this->_transformation.multiply(&transformation_data.matrix);
+
+  // Move back to its origin
+  move_to_center[3][0] = center.getx();
+  move_to_center[3][1] = center.gety();
+  move_to_center[3][2] = center.getz();
+
+  this->_transformation.multiply(&move_to_center);
 }
 
+void Transformation::_set_rotation_data(TransformationData &transformation_data, unsigned int &index, Coordinate &center)
+{
+  switch(transformation_data.rotation_type)
+  {
+    case RotationType::ON_WORLD_CENTER:
+    {
+      // Just rotate it, as all rotations are based on the world center
+      if( index == 0 )
+      {
+        this->_transformation = transformation_data.matrix;
+      }
+      else
+      {
+        this->_transformation.multiply(&transformation_data.matrix);
+      }
+      break;
+    }
+
+    case RotationType::ON_ITS_OWN_CENTER:
+    {
+      this->_rotation_on_center(transformation_data, index, center);
+      break;
+    }
+
+    case RotationType::ON_GIVEN_COORDINATE:
+    {
+      this->_rotation_on_coordinate(transformation_data, index, center);
+      break;
+    }
+
+    default:
+    {
+      LOG(1, "");
+      LOG(1, "");
+      LOG(1, "ERROR! Invalid RotationType used: %d", transformation_data.rotation_type);
+    }
+  }
+}
+
+void Transformation::_rotation_on_center(TransformationData &transformation_data, unsigned int &index, Coordinate &center)
+{
+  MatrixForm move_to_center
+  {
+    {1             ,  0            ,              0},
+    {0             ,  1            ,              0},
+    {-center.getx(), -center.gety(), -center.getz()}
+  };
+
+  if( index == 0 )
+  {
+    this->_transformation = move_to_center;
+  }
+  else
+  {
+    this->_transformation.multiply(&move_to_center);
+  }
+
+  // Do the rotation on the origin
+  this->_transformation.multiply(&transformation_data.matrix);
+
+  // Move back to its origin
+  move_to_center[3][0] = center.getx();
+  move_to_center[3][1] = center.gety();
+  move_to_center[3][2] = center.getz();
+
+  this->_transformation.multiply(&move_to_center);
+}
+
+
+void Transformation::_rotation_on_coordinate(TransformationData &transformation_data, unsigned int &index, Coordinate &center)
+{
+  auto rotation_center = transformation_data.rotation_center;
+
+  MatrixForm move_to_center
+  {
+    {1                      ,  0                     ,                      0},
+    {0                      ,  1                     ,                      0},
+    {-rotation_center.getx(), -rotation_center.gety(),-rotation_center.getz()}
+  };
+
+  if( index == 0 )
+  {
+    this->_transformation =move_to_center;
+  }
+  else
+  {
+    this->_transformation.multiply(&move_to_center);
+  }
+
+  // Do the rotation on the origin
+  this->_transformation.multiply(&transformation_data.matrix);
+
+  // Move back to its origin
+  move_to_center[3][0] = rotation_center.getx();
+  move_to_center[3][1] = rotation_center.gety();
+  move_to_center[3][2] = rotation_center.getz();
+
+  this->_transformation.multiply(&move_to_center);
+}
