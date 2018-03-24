310ce0157e10077538ce4d7ad8fb49aad22cde5d
evandrocoan <evandrocoan@hotmail.com>
2018-03-24 20:25:19 -0300

    Small documentation and debugging statement changes

--- src/viewport.h
+++ src/viewport.h
@@ -12,6 +12,18 @@
  *
  * Drawing Straight Lines
  * https://developer.gnome.org/gtkmm-tutorial/stable/sec-cairo-drawing-lines.html.en#cairo-example-lines
+ *
+ * Viewport: a Porta de Visualização é a área do vídeo
+ * onde será desenhado o que se deseja mostrar.
+ * – Representado num sistema de coordenadas parcialmente
+ * invertido, onde o eixo Y aponta para baixo.
+ * – Tamanho fixo ou não controlado pela aplicação.
+ *
+ * The ViewPort is the computer screen which has the (0, 0) start point on the upper-left point,
+ * instead of the down-left point as usual. And there, is where the ViewWindow has its starts point.
+ *
+ * And beyond these two systems, there is the world which has its start point symmetric with the
+ * ViewWindow.
  */
 class ViewPort
 {
