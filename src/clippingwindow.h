72cd981303c0fb6a3c858136ac2b16f8c4333750
evandrocoan <evandrocoan@hotmail.com>
2018-03-25 22:34:21 -0300

    Split transformationdata.h and clippingwindow.h to .cpp files

--- src/clippingwindow.h
+++ src/clippingwindow.h
@@ -6,27 +6,8 @@
 class ClippingWindow : public Array<4, Coordinate*>
 {
 public:
-  ClippingWindow()
-  {
-      this->_data[0] = new Coordinate(0, 0, 1);
-      this->_data[1] = new Coordinate(0, 0, 1);
-      this->_data[2] = new Coordinate(0, 0, 1);
-      this->_data[3] = new Coordinate(0, 0, 1);
-
-      this->x1 = {this->_data[1], this->_data[2]};
-      this->y1 = {this->_data[0], this->_data[1]};
-      this->x2 = {this->_data[3], this->_data[0]};
-      this->y2 = {this->_data[2], this->_data[3]};
-  }
-
-  ~ClippingWindow()
-  {
-  }
-
-  Coordinate* getPoint(unsigned int index)
-  {
-    return this->_data[index];
-  }
+  ClippingWindow();
+  ~ClippingWindow();
 
   /**
    * Lines representing the clipping window saved on viewport coordinates:
@@ -85,31 +66,11 @@ public:
    *          +-------------------+
    * firstCoordinate        forthCoordinate
    */
-  void updateClippingWindowSize(int width, int height)
-  {
-    (*this->_data[0])[0] = clipping_window_margin_distance;
-    (*this->_data[0])[1] = clipping_window_margin_distance;
+  void updateClippingWindowSize(int width, int height);
 
-    (*this->_data[1])[0] = clipping_window_margin_distance;
-    (*this->_data[1])[1] = height - clipping_window_margin_distance;
+  Coordinate* getPoint(unsigned int index);
 
-    (*this->_data[2])[0] = width - clipping_window_margin_distance;
-    (*this->_data[2])[1] = height - clipping_window_margin_distance;
-
-    (*this->_data[3])[0] = width - clipping_window_margin_distance;
-    (*this->_data[3])[1] = clipping_window_margin_distance;
-  }
-
-  friend std::ostream& operator<<(std::ostream &output, const ClippingWindow &object)
-  {
-    output << "ClippingWindow" << "["
-        << "x1" << *object.x1[0] << *object.x1[1] << " "
-        << "y1" << *object.y1[0] << *object.y1[1] << " "
-        << "x2" << *object.x2[0] << *object.x2[1] << " "
-        << "y2" << *object.y2[0] << *object.y2[1] << "]";
-
-    return output;
-  }
+  friend std::ostream& operator<<(std::ostream &output, const ClippingWindow &object);
 };
 
 #endif // GTKMM_APP_AXES
