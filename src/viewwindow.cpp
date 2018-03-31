0531d6f0153bfb18cf4a6f9c01d7ebef60baac86
evandrocoan <evandrocoan@hotmail.com>
2018-03-31 12:31:11 -0300

    Simplified the viewwindow.cpp rotation code

--- src/viewwindow.cpp
+++ src/viewwindow.cpp
@@ -1,9 +1,8 @@
 #include "viewwindow.h"
 
 ViewWindow::ViewWindow() :
-      _width(100),
-      _height(100),
-      _angle(0, 0),
+      _angles(0, 0),
+      _dimentions(100, 100),
       _windowCenter(100, 100)
 {
 }
@@ -14,18 +13,18 @@ ViewWindow::~ViewWindow()
 
 long double ViewWindow::width()
 {
-  return this->_width;
+  return this->_dimentions[0];
 }
 
 long double ViewWindow::height()
 {
-  return this->_height;
+  return this->_dimentions[1];
 }
 
 std::ostream& operator<<( std::ostream &output, const ViewWindow &object )
 {
-  output << "ViewWindow[" << object._width << "x" << object._height << "]"
-      << object._windowCenter << " " << object._transformation;
+  output << "ViewWindow[" << object._dimentions << "]"
+      << object._windowCenter << " " << object._transformation << " " << object._angle_rotation;
   return output;
 }
 
@@ -36,16 +35,20 @@ Signal<>::Connection ViewWindow::addObserver(const Signal<>::Callback &callback)
   return connection;
 }
 
-void ViewWindow::zoom(long int step)
+void ViewWindow::zoom(Coordinate step)
 {
-  if( (this->_width + step <= MINIMUM_ZOOM_LIMIT || this->_height + step <= MINIMUM_ZOOM_LIMIT) && step < 0 )
+  if( (this->_dimentions[0] + step[0] <= MINIMUM_ZOOM_LIMIT
+      || this->_dimentions[1] + step[0] <= MINIMUM_ZOOM_LIMIT)
+    && step[0] < 0 )
   {
     LOG(1, "");
     LOG(1, "");
     LOG(1, "ERROR: You reached the maximum zoom limit! %s", *this);
     return;
   }
-  else if( (this->_width + step >= MAXIMUM_ZOOM_LIMIT || this->_height + step >= MAXIMUM_ZOOM_LIMIT) && step > 0)
+  else if( (this->_dimentions[0] + step[0] >= MAXIMUM_ZOOM_LIMIT
+            || this->_dimentions[1] + step[0] >= MAXIMUM_ZOOM_LIMIT)
+          && step[0] > 0)
   {
     LOG(1, "");
     LOG(1, "");
@@ -54,22 +57,24 @@ void ViewWindow::zoom(long int step)
   }
   else
   {
-    this->_width += step;
-    this->_height += step;
+    this->_dimentions += step;
     this->callObservers();
   }
 }
 
-void ViewWindow::move(long int horizontal_step, long int vertical_step)
+void ViewWindow::move(Coordinate movement)
 {
-  this->_windowCenter[0] += horizontal_step;
-  this->_windowCenter[1] += vertical_step;
+  this->_angle_rotation.clear();
+  this->_angle_rotation.add_rotation("Transformation on the ViewWindow by Rotation", this->_angles);
+  this->_angle_rotation.apply(movement);
+
+  this->_windowCenter += movement;
   this->callObservers();
 }
 
-void ViewWindow::rotate(Coordinate _angle)
+void ViewWindow::rotate(Coordinate angles)
 {
-  this->_angle += -_angle;
+  this->_angles += -angles;
   this->callObservers();
 }
 
@@ -77,11 +82,10 @@ void ViewWindow::callObservers()
 {
   this->_transformation.clear();
   this->_transformation.add_translation("Translation to wold center", -this->_windowCenter);
-  this->_transformation.add_rotation("Rotation on given coordinate", this->_angle);
-  this->_transformation.add_scaling("Scaling for window coordinates", Coordinate(1.0/this->_width, 1.0/this->_height, 1.0));
+  this->_transformation.add_rotation("Rotation on given coordinate", this->_angles);
+  this->_transformation.add_scaling("Scaling for window coordinates", this->_dimentions.inverse());
   this->_transformation.set_geometric_center();
-  LOG(4, "_width: %s", this->_width);
-  LOG(4, "1/this->_width: %s", 1.0/this->_width);
+  LOG(4, "_width: %s, 1/this->_width: %s", this->_dimentions, this->_dimentions.inverse());
   LOG(4, "_transformation: %s", _transformation);
   this->_observers();
 }
