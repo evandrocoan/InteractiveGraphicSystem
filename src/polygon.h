bbbb50915cc832047e63d4a4f2ebcac02c05fdcc
evandrocoan <evandrocoan@hotmail.com>
2018-04-03 16:27:05 -0300

    Implemented Polygon Clipping on src/polygon.cpp

--- src/polygon.h
+++ src/polygon.h
@@ -9,6 +9,22 @@ class Polygon : public DrawableObject
 public:
   Polygon(std::string name, std::list<Coordinate*> coordinates);
   ~Polygon();
+
+  virtual void updateClippingCoordinates(const Axes&);
+
+protected:
+  /**
+   * http://www.pracspedia.com/CG/sutherlandhodgman.html
+   * https://en.wikipedia.org/wiki/Sutherland%E2%80%93Hodgman_algorithm
+   * https://github.com/nogenem/UFSC_CG-2015-2/blob/master/Projeto_1/Etapa_4/include/Clipping.hpp
+   *
+   * @return true if the line should be drawn, false if the line should be skipped drawing
+   */
+  bool _sutherlandHodgmanPolygonClip(const Axes&);
+  void _sutherlandHodgmanClipLeft(const Axes& axes, std::vector<Coordinate>& input, std::vector<Coordinate>& temporary);
+  void _sutherlandHodgmanClipRight(const Axes& axes, std::vector<Coordinate>& temporary, std::vector<Coordinate>& clippingResult);
+  void _sutherlandHodgmanClipTop(const Axes& axes, std::vector<Coordinate>& clippingResult, std::vector<Coordinate>& temporary);
+  void _sutherlandHodgmanClipBottom(const Axes& axes, std::vector<Coordinate>& temporary, std::vector<Coordinate>& clippingResult);
 };
 
 #endif // GTKMM_APP_POLYGON
