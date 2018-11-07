daf8423e6299f297fc1e32980eed61fa1aaa097b
evandrocoan <evandrocoan@hotmail.com>
2018-11-07 21:13:29 -0200

    Created the facade.cpp for creating basic forms tests

---
+++ src/facade.cpp
@@ -0,0 +1,21 @@
+
+#include "facade.h"
+
+void Facade::add_test_objects()
+{
+  std::vector<big_double> polygon_coord_list;
+
+  // polygon_coord_list.clear();
+  // polygon_coord_list.push_back(0); polygon_coord_list.push_back(0); polygon_coord_list.push_back(1);
+  // polygon_coord_list.push_back(0); polygon_coord_list.push_back(20); polygon_coord_list.push_back(1);
+  // polygon_coord_list.push_back(20); polygon_coord_list.push_back(20); polygon_coord_list.push_back(1);
+  // polygon_coord_list.push_back(20); polygon_coord_list.push_back(0); polygon_coord_list.push_back(1);
+  // this->addPolygon("Polygon", polygon_coord_list, _default_coordinate_value_parameter, _default_coordinate_value_parameter, CurveType::POLYGON);
+
+  polygon_coord_list.clear();
+  polygon_coord_list.push_back(0); polygon_coord_list.push_back(0); polygon_coord_list.push_back(1);
+  polygon_coord_list.push_back(50); polygon_coord_list.push_back(50); polygon_coord_list.push_back(1);
+  polygon_coord_list.push_back(100); polygon_coord_list.push_back(50); polygon_coord_list.push_back(1);
+  polygon_coord_list.push_back(100); polygon_coord_list.push_back(0); polygon_coord_list.push_back(1);
+  this->addPolygon("Bezier", polygon_coord_list, _default_coordinate_value_parameter, _default_coordinate_value_parameter, CurveType::BEZIER);
+}
