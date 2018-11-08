0a501c796ecf6250a2604b1ca30d9b956909563e
evandrocoan <evandrocoan@hotmail.com>
2018-11-08 14:57:02 -0200

    Created a polygon example on facade.cpp

--- src/facade.cpp
+++ src/facade.cpp
@@ -5,12 +5,12 @@ void Facade::add_test_objects()
 {
   std::vector<big_double> polygon_coord_list;
 
-  // polygon_coord_list.clear();
-  // polygon_coord_list.push_back(0); polygon_coord_list.push_back(0); polygon_coord_list.push_back(1);
-  // polygon_coord_list.push_back(0); polygon_coord_list.push_back(20); polygon_coord_list.push_back(1);
-  // polygon_coord_list.push_back(20); polygon_coord_list.push_back(20); polygon_coord_list.push_back(1);
-  // polygon_coord_list.push_back(20); polygon_coord_list.push_back(0); polygon_coord_list.push_back(1);
-  // this->addPolygon("Polygon", polygon_coord_list, _default_coordinate_value_parameter, _default_coordinate_value_parameter, CurveType::POLYGON);
+  polygon_coord_list.clear();
+  polygon_coord_list.push_back(-50); polygon_coord_list.push_back(-50); polygon_coord_list.push_back(1);
+  polygon_coord_list.push_back(-50); polygon_coord_list.push_back(-30); polygon_coord_list.push_back(1);
+  polygon_coord_list.push_back(-30); polygon_coord_list.push_back(-30); polygon_coord_list.push_back(1);
+  polygon_coord_list.push_back(-30); polygon_coord_list.push_back(-50); polygon_coord_list.push_back(1);
+  this->addPolygon("Polygon", polygon_coord_list, _default_coordinate_value_parameter, _default_coordinate_value_parameter, CurveType::POLYGON);
 
   polygon_coord_list.clear();
   polygon_coord_list.push_back(0); polygon_coord_list.push_back(0); polygon_coord_list.push_back(1);
