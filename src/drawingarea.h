495d4154547a767f28135deba80929795c483364
evandrocoan <evandrocoan@hotmail.com>
2018-11-11 01:06:11 -0200

    Created a specialized list of points to draw on world.cpp

--- src/drawingarea.h
+++ src/drawingarea.h
@@ -56,10 +56,10 @@ protected:
   void _draw_clipping_axes(const Cairo::RefPtr<Cairo::Context>&);
 
 private:
-  void drawn_circle(const Cairo::RefPtr<Cairo::Context>&, const Curve*);
-  void drawn_general(const Cairo::RefPtr<Cairo::Context>&, const DrawableObject*);
-
+  void drawn_line(const Cairo::RefPtr<Cairo::Context>&, const DrawableObject*);
   void drawn_point(const Cairo::RefPtr<Cairo::Context>&, const DrawableObject*);
+
+  void drawn_circle(const Cairo::RefPtr<Cairo::Context>&, const Curve*);
   void drawn_polygon(const Cairo::RefPtr<Cairo::Context>&, const DrawableObject*);
 };
 
