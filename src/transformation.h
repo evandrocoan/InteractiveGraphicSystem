3700ce5573a07b6367190b71cfecdbe1b584484a
evandrocoan <evandrocoan@hotmail.com>
2018-11-23 22:25:17 -0200

    Merged transformationdata.h into transformation.h

--- src/transformation.h
+++ src/transformation.h
@@ -17,12 +17,53 @@
 
 #include "coordinate.h"
 #include "matrixform.h"
-#include "transformationdata.h"
 
 enum Projection
 {
   PARALLEL,
   PERSPECTIVE
+};
+
+enum TransformationPoint
+{
+  ON_WORLD_CENTER,
+  ON_ITS_OWN_CENTER,
+  ON_GIVEN_COORDINATE
+};
+
+enum TransformationType
+{
+  SCALING,
+  ROTATION,
+  TRANSLATION
+};
+
+std::ostream& operator<<(std::ostream &output, const TransformationPoint object);
+std::ostream& operator<<(std::ostream &output, const TransformationType object);
+
+/**
+ * Ignoring the `translations` matrices, the `matrix` is the main operation applied after
+ * moving the object to the world center.
+ */
+struct TransformationData
+{
+  std::string name;
+  MatrixForm matrix;
+  TransformationType type;
+
+  Coordinate center;
+  TransformationPoint point;
+
+  /**
+   * This `center` default value is null, because it is only used when the object is a
+   * rotation which should be performed around some specific coordinate as the world center, instead
+   * of the object geometric center.
+   */
+  TransformationData(std::string name, MatrixForm matrix, TransformationType type,
+      TransformationPoint point = TransformationPoint::ON_WORLD_CENTER,
+      Coordinate center = _origin_coordinate_value);
+
+  friend std::ostream& operator<<(std::ostream &output, const TransformationData &object);
 };
 
 class Transformation
